031656695f59224f1c99378ce6b5a29dbb13a33c
Alex <alp_papps@hotmail.com>
2013-11-09 18:29:18 -0500

    Add in dummy states.

--- processor/FSM.v
+++ processor/FSM.v
@@ -1,6 +1,6 @@
 /******************************************************************************
  * File: FSM.v
- * Author: ECE243 development team, Patrick Payne, Alex Papanicolaou
+ * Author: ECE352 development team, Patrick Payne, Alex Papanicolaou
  * Date Created: Original 2007, modifications November 2013
  * Purpose: Implements the control of the multicycle processor. See project
  *    documentation for details.
@@ -17,12 +17,13 @@ module FSM (
     output reg [2:0] ALU2, ALUop);
 
   /* The 4 bit number representing the state of the control. */
-  reg [3:0] state;
+  reg [4:0] state;
  
   /* state constants (note: asn = add/sub/nand, asnsh = add/sub/nand/shift) */
-  parameter [3:0] reset_s = 0, c1 = 1, c2 = 2, c3_asn = 3, c4_asnsh = 4,
+  parameter [4:0] reset_s = 0, c1 = 1, c2 = 2, c3_asn = 3, c4_asnsh = 4,
      c3_shift = 5, c3_ori = 6, c4_ori = 7, c5_ori = 8, c3_load = 9,
-     c4_load = 10, c3_store = 11, c3_bpz = 12, c3_bz = 13, c3_bnz = 14, c3_stop = 15;
+     c4_load = 10, c3_store = 11, c3_bpz = 12, c3_bz = 13, c3_bnz = 14, c3_stop = 15,
+     c3_dummy = 16, c4_dummy = 17, c5_dummy = 18;
   
   /* Define constants for the different possible opcodes. */
   parameter [2:0] i_shift = 3, i_ori = 7;
@@ -57,23 +58,26 @@ module FSM (
           else if(instr == i_bpz) state = c3_bpz;
           else if(instr == i_bz) state = c3_bz;
           else if(instr == i_bnz) state = c3_bnz;
-			 else if(instr == i_nop) state = c1;
+			 else if(instr == i_nop) state = c3_dummy;
 			 else if(instr == i_stop) state = c3_stop;
           else state = 0;
         end
         c3_asn: state = c4_asnsh; // cycle 3: ADD SUB NAND
-        c4_asnsh: state = c1; // cycle 4: ADD SUB NAND/SHIFT
+        c4_asnsh: state = c5_dummy; // cycle 4: ADD SUB NAND/SHIFT
         c3_shift: state = c4_asnsh; // cycle 3: SHIFT
         c3_ori: state = c4_ori; // cycle 3: ORI
         c4_ori: state = c5_ori; // cycle 4: ORI
         c5_ori: state = c1; // cycle 5: ORI
         c3_load: state = c4_load; // cycle 3: LOAD
-        c4_load: state = c1; // cycle 4: LOAD
-        c3_store: state = c1; // cycle 3: STORE
-        c3_bpz: state = c1; // cycle 3: BPZ
-        c3_bz: state = c1; // cycle 3: BZ
-        c3_bnz: state = c1; // cycle 3: BNZ
-		  c3_stop: state = c3_stop; // cycle 3: STOP
+        c4_load: state = c5_dummy; // cycle 4: LOAD
+        c3_store: state = c4_dummy; // cycle 3: STORE
+        c3_bpz: state = c4_dummy; // cycle 3: BPZ
+        c3_bz: state = c4_dummy; // cycle 3: BZ
+        c3_bnz: state = c4_dummy; // cycle 3: BNZ
+		  c3_stop: state = c4_dummy; // cycle 3: STOP
+      c3_dummy: state = c4_dummy; // cycle 3: ANY
+      c4_dummy: state = c5_dummy; // cycle 4: ANY
+      c5_dummy: state = c1; // cycle 5: ANY
       endcase
     end
   end
@@ -407,6 +411,60 @@ module FSM (
         FlagWrite = 0;
 		  Stop = 1;
       end
+    c3_dummy: begin //control = 19'b0000000000000000000;
+        PC_sel = 0;
+        PCwrite = 0;
+        MemRead = 0;
+        MemWrite = 0;
+        IRload = 0;
+        R1Sel = 0;
+        MDRload = 0;
+        R1R2Load = 0;
+        ALU1 = 0;
+        ALU2 = ALU2_R2;
+        ALUop = ALUop_add;
+        ALUOutWrite = 0;
+        RFWrite = 0;
+        RegIn = 0;
+        FlagWrite = 0;
+		  Stop = 0;
+      end 
+    c4_dummy: begin //control = 19'b0000000000000000000;
+        PC_sel = 0;
+        PCwrite = 0;
+        MemRead = 0;
+        MemWrite = 0;
+        IRload = 0;
+        R1Sel = 0;
+        MDRload = 0;
+        R1R2Load = 0;
+        ALU1 = 0;
+        ALU2 = ALU2_R2;
+        ALUop = ALUop_add;
+        ALUOutWrite = 0;
+        RFWrite = 0;
+        RegIn = 0;
+        FlagWrite = 0;
+		  Stop = 0;
+      end 
+    c5_dummy: begin //control = 19'b0000000000000000000;
+        PC_sel = 0;
+        PCwrite = 0;
+        MemRead = 0;
+        MemWrite = 0;
+        IRload = 0;
+        R1Sel = 0;
+        MDRload = 0;
+        R1R2Load = 0;
+        ALU1 = 0;
+        ALU2 = ALU2_R2;
+        ALUop = ALUop_add;
+        ALUOutWrite = 0;
+        RFWrite = 0;
+        RegIn = 0;
+        FlagWrite = 0;
+		  Stop = 0;
+      end 
       default: begin //control = 19'b0000000000000000000;
         PC_sel = 0;
         PCwrite = 0;
